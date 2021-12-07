e92f2d7e227825c66c971ed1fbc93a216758d1b5
jobel johny <jobeljohny@gmail.com>
2021-12-07 13:02:55 +0530

    added fabric functionality and events

--- src/app/canvas/canvas.component.ts
+++ src/app/canvas/canvas.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { fabric } from 'fabric';
 
 @Component({
   selector: 'app-canvas',
@@ -7,9 +8,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CanvasComponent implements OnInit {
 
+  canvas:any;
+  colors:string[]=["red","green","blue","orange","black","yellow"]
   constructor() { }
 
   ngOnInit(): void {
+    // this.canvas = new fabric.Canvas('canvas');
+    //  this.canvas.add(new fabric.IText('Hello World !'));
   }
+  ngAfterViewInit() {
+     this.canvas = new fabric.Canvas('canvas');
+    
+}
+getAdder(offset:number):number{
+ return  Math.floor(Math.random() * offset);
+}
+
+addShape():void{
+  var color=this.colors[Math.floor(Math.random() * this.colors.length)]
+  const rect = new fabric.Rect({
+    top : 100+this.getAdder(100),
+    left : 100+this.getAdder(100),
+    width : 60+this.getAdder(40),
+    height : 70+this.getAdder(40),
+    fill : color
+ });
+ this.canvas.add(rect);
+
+}
 
 }
