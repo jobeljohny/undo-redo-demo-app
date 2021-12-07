bff3f82726e7349e85e70e9a3ccf00837c0d0254
jobel johny <jobeljohny@gmail.com>
2021-12-07 12:04:52 +0530

    added canvas layout and menus UI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CanvasComponent } from './canvas/canvas.component';
+import { MenusComponent } from './menus/menus.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CanvasComponent,
+    MenusComponent
   ],
   imports: [
     BrowserModule,
